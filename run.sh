6639d854da6d34c8e3d351c1ea1d6a4cab7e7982
my282 <myoumyou1228@gmail.com>
2026-03-18 13:28:43 +0900

    add: add automatic build into run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,9 @@ set -xue
 
 QEMU=qemu-system-riscv32
 
+cd kernel
+cargo build
+cd ..
+
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
   -kernel ./kernel/target/riscv32imac-unknown-none-elf/debug/kernel
